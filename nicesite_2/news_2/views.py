e88a2270cbcfa3589d034e968773a4061f286027
MikoJIa <Kolya_popov_86@tut.by>
2023-09-17 19:50:35 +0300

    add migrate, templates index.html, models, views.py

--- nicesite_2/news_2/views.py
+++ nicesite_2/news_2/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    return HttpResponse(f"<h1>Hello world!!!!!</h1>")
+    news = News.objects.all()
+    context = {
+        'news': news,
+        'title': 'Список новостей'
+    }
+    return render(request, 'news_2/index.html', context)
+    # return render(request, 'news_2/index.html')
 
 
 def test(request):
